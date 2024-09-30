d316ee11b8289cb578b9d5efd9a815970e16e5a2
Mark Muetzelfeldt <markmuetz@gmail.com>
2024-09-30 11:59:03 +0100

    Add u-dj618 to transferred checks.

--- ctrl/scripts/check_monsoon_jasmin_tx.py
+++ ctrl/scripts/check_monsoon_jasmin_tx.py
@@ -18,6 +18,16 @@ for suite, case, var in product(suites, cases, um_vars):
     path = simdir / f'{suite}/share/cycle/{case}/engl/um/englaa_pa.merged.{case}.{suite}.{var}.nc'
     merged_paths.append(path)
 
+merged_paths.extend([
+    simdir / f'u-dj618/share/cycle/20200701T0000Z/engl/um/englaa_pa.merged.20200701T0000Z.u-dj618.{var}.nc'
+    for var in um_vars
+])
+
+not_transferred = []
 for path in merged_paths:
     if not path.exists():
         print('Not transferred yet:', path)
+        not_transferred.append(path)
+
+if not not_transferred:
+    print('All files successfully transferred')
