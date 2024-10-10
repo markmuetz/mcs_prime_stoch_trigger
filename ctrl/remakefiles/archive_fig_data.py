1d448b749677349478f7dbfb7f46032f191e1d0d
Mark Muetzelfeldt <markmuetz@gmail.com>
2024-10-10 11:19:37 +0100

    Only archive fig data (full data taking ages!).

--- ctrl/remakefiles/archive_fig_data.py
+++ ctrl/remakefiles/archive_fig_data.py
@@ -1,7 +1,7 @@
 from remake import ArchiveV1, ArchiveV1Rule
 
 archive = ArchiveV1(
-    name='mcs_prime_stoch_trigger',
+    name='mcs_prime_stoch_trigger_fig_data',
     git_repo_url='http://github.com/markmuetz/mcs_prime_stoch_trigger',
     remakefile='N216_ens_analysis.py',
     author='Mark Muetzelfeldt',
@@ -9,13 +9,16 @@ archive = ArchiveV1(
     archive_loc='/gws/nopw/j04/mcs_prime/mmuetz/tmp',
 )
 
-archive.add(
-    ArchiveV1Rule(
-        'GuassianFilterN216Imerg',
-        inputs='all',
-))
-archive.add(
-    ArchiveV1Rule(
-        'GuassianFilterExpt',
-        inputs='all',
-))
+rules = [
+    'PlotTotalPrecip',
+    'PlotSpreadSkill',
+    'PlotAutocorr',
+    'PlotTCWV',
+    'PlotMCSPCallingFreq',
+]
+for rule_name in rules:
+    archive.add(
+        ArchiveV1Rule(
+            rule_name,
+            inputs='all',
+    ))
