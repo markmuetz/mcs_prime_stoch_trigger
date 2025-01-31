ec91acf657722455aa94eec24f4f975fa3b8e9af
Mark Muetzelfeldt <markmuetz@gmail.com>
2025-01-31 16:45:11 +0000

    Just rerun processing for u-dj618/reduced calling freq.

--- monsoon/N216_ens_postproc/rsync_sims_to_jasmin.sh
+++ monsoon/N216_ens_postproc/rsync_sims_to_jasmin.sh
@@ -18,5 +18,5 @@ else
 fi
 
 cd /projects/mcsprime/mamue/cylc-run
-rsync -Rav --progress ./{u-dg135,u-di727,u-di728,u-dj618}/share/cycle/2020??01T0000Z/engl/um/englaa_p[ab].merged.2020??01T0000Z.*.nc mmuetz@xfer3.jasmin.ac.uk:/gws/nopw/j04/mcs_prime/mmuetz/data/UM_sims/
+rsync -Rav --progress ./{u-dg135,u-di727,u-di728,u-dj618}/share/cycle/2020??01T0000Z/engl/um/englaa_p[ab].merged.2020??01T0000Z.*.nc mmuetz@xfer-vm-02.jasmin.ac.uk:/gws/nopw/j04/mcs_prime/mmuetz/data/UM_sims/
 cd -
